650c1c46d0f9f7cf22310a7a6982493a0b41b009
Helaman Franco <tu_correo@ejemplo.com>
2026-09-23 23:02:53 -0500

    Week 01

--- server.js
+++ server.js
@@ -42,6 +42,10 @@ app.get('/organizations', (req, res) => {
   res.render('organizations', { title: 'Organizations' });
 });
 
+app.get('/categories', (req, res) => {
+  res.render('categories', { title: 'Categories' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://127.0.0.1:${PORT}`);
   console.log(`Environment: ${NODE_ENV}`);
