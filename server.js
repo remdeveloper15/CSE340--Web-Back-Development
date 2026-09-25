ff7e7d64b3dc6dd813c596bf5f6535b0a2f15919
Helaman Franco <tu_correo@ejemplo.com>
2026-09-24 21:41:41 -0500

    Week 02

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { testConnection} from './src/models/db.js'; 
+import { getAllOrganizations } from './src/models/organizations.js';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -38,15 +40,23 @@ app.get('/projects', (req, res) => {
   res.render('projects', { title: 'Projects' });
 });   
 
-app.get('/organizations', (req, res) => {
-  res.render('organizations', { title: 'Organizations' });
+app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
+      
+    const title = 'Our Partner Organizations';
+    res.render('organizations', { title, organizations });
 });
 
 app.get('/categories', (req, res) => {
   res.render('categories', { title: 'Categories' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://127.0.0.1:${PORT}`);
-  console.log(`Environment: ${NODE_ENV}`);
+app.listen(PORT, async () => {
+  try {
+    await testConnection();
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
 });
